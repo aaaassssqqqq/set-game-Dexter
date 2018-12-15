2621b83fd7c3869787050f6e70724091ff697baa
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-12-15 11:57:47 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`مهلبية بلعسل.`,'https://www.twitch.tv/v5bz');
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
+});
+
+client.login(process.env.BOT_TOKEN);
